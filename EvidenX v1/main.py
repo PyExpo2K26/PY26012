9b5da2ba54e23a0e69cb2bdcce543a9b7435fb47
KnightNode <thestoicway007@gmail.com>
2026-02-06 09:15:51 +0530

    Update main.py

--- EvidenX v1/main.py
+++ EvidenX v1/main.py
@@ -10,3 +10,9 @@ from engine.ela import perform_ela
 from engine.metadata import extract_metadata
 from engine.copymove import detect_copymove
 from engine.cnn import predict_cnn, load_model
+
+app = FastAPI()
+app.mount("/source", StaticFiles(directory="source"), name="source")
+@app.on_event("startup")
+async def startup_event():
+    load_model()
