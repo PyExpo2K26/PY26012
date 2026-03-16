6bdbde91712a6033080f33eaeb8e20ff21c4ef4d
Ajay23-svg482 <ajaysara9201@gmail.com>
2026-03-17 03:18:09 +0530

    Create copymove.py

---
+++ source/EvidenX v1/copymove.py
@@ -0,0 +1,11 @@
+import cv2
+import numpy as np
+
+def detect_copymove(image_path):
+    try:
+        img = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+        if img is None:
+            return 0, 0.0
+
+
+        orb = cv2.ORB_create()
