80fdc7cfc7e7122631f12e6ef400e6891ea19191
Ajay23-svg482 <ajaysara9201@gmail.com>
2026-03-17 03:21:29 +0530

    Update copymove.py

--- source/EvidenX v1/copymove.py
+++ source/EvidenX v1/copymove.py
@@ -17,4 +17,29 @@ def detect_copymove(image_path):
   
         bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=False)
         matches = bf.knnMatch(descriptors, descriptors, k=2)
+        good_matches = []
+        for m, n in matches:
+  
+            if m.distance < 0.75 * n.distance:
+  
+                pt1 = keypoints[m.queryIdx].pt
+                pt2 = keypoints[n.queryIdx].pt
+                
+                spatial_dist = np.sqrt((pt1[0]-pt2[0])**2 + (pt1[1]-pt2[1])**2)
+                
+   
+                if spatial_dist > 20: 
+                    good_matches.append(m)
+        
+        count = len(good_matches)
+        
+     
+        score = min(count / 50.0, 1.0) 
+        
+        return count, float(score)
+
+    except Exception as e:
+        print(f"Copy-Move Error: {e}")
+        return 0, 0.0
+
         
