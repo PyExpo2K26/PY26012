0242ec8793b23d2fc4163a5c42f232e6cf27619c
HarshiniRavichandran18 <harshini4069@gmail.com>
2026-03-17 04:01:05 +0530

    Clean up comments in GAN model classes
    Removed comments indicating state sizes in the Generator and Discriminator classes.

--- source/EvidenX v3/engine/gan.py
+++ source/EvidenX v3/engine/gan.py
@@ -5,26 +5,20 @@ class Generator(nn.Module):
     def __init__(self, nz=100, ngf=64, nc=3):
         super(Generator, self).__init__()
         self.main = nn.Sequential(
-            # input is Z, going into a convolution
             nn.ConvTranspose2d(nz, ngf * 8, 4, 1, 0, bias=False),
             nn.BatchNorm2d(ngf * 8),
             nn.ReLU(True),
-            # state size. (ngf*8) x 4 x 4
             nn.ConvTranspose2d(ngf * 8, ngf * 4, 4, 2, 1, bias=False),
             nn.BatchNorm2d(ngf * 4),
             nn.ReLU(True),
-            # state size. (ngf*4) x 8 x 8
             nn.ConvTranspose2d(ngf * 4, ngf * 2, 4, 2, 1, bias=False),
             nn.BatchNorm2d(ngf * 2),
             nn.ReLU(True),
-            # state size. (ngf*2) x 16 x 16
             nn.ConvTranspose2d(ngf * 2, ngf, 4, 2, 1, bias=False),
             nn.BatchNorm2d(ngf),
             nn.ReLU(True),
-            # state size. (ngf) x 32 x 32
             nn.ConvTranspose2d(ngf, nc, 4, 2, 1, bias=False),
             nn.Tanh()
-            # state size. (nc) x 64 x 64
         )
 
     def forward(self, input):
@@ -34,22 +28,17 @@ class Discriminator(nn.Module):
     def __init__(self, nc=3, ndf=64):
         super(Discriminator, self).__init__()
         self.main = nn.Sequential(
-            # input is (nc) x 64 x 64
             nn.Conv2d(nc, ndf, 4, 2, 1, bias=False),
             nn.LeakyReLU(0.2, inplace=True),
-            # state size. (ndf) x 32 x 32
             nn.Conv2d(ndf, ndf * 2, 4, 2, 1, bias=False),
             nn.BatchNorm2d(ndf * 2),
             nn.LeakyReLU(0.2, inplace=True),
-            # state size. (ndf*2) x 16 x 16
             nn.Conv2d(ndf * 2, ndf * 4, 4, 2, 1, bias=False),
             nn.BatchNorm2d(ndf * 4),
             nn.LeakyReLU(0.2, inplace=True),
-            # state size. (ndf*4) x 8 x 8
             nn.Conv2d(ndf * 4, ndf * 8, 4, 2, 1, bias=False),
             nn.BatchNorm2d(ndf * 8),
             nn.LeakyReLU(0.2, inplace=True),
-            # state size. (ndf*8) x 4 x 4
             nn.Conv2d(ndf * 8, 1, 4, 1, 0, bias=False),
             nn.Sigmoid()
         )
@@ -58,7 +47,6 @@ class Discriminator(nn.Module):
         return self.main(input)
 
 def load_gan_model():
-    # Placeholder for loading weights
     netD = Discriminator()
     netG = Generator()
     netD.eval()
