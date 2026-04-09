734a167b81cb1432b4cbb835e626b1de16c48b10
Mrudul P Manesh <mrudulpmanesh2007@gmail.com>
2026-04-09 20:49:21 +0530

    Delete source/EvidenX v1 directory

--- source/EvidenX v1/run_app.py
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Installing dependencies (if needed)...
-python -m pip install -r requirements.txt
-echo.
-echo Starting EvidenX Backend...
-echo Open your browser to: http://localhost:8000
-echo.
-python main.py
-pause
